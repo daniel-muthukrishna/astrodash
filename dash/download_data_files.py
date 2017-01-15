3bd42909f942575b0adef16dc924ef3ae824ade3
dmuthukrishna <daniel.muthukrishna@gmail.com>
2017-01-15 19:00:23 -0300

    Compatible on Python2 and Python 3 for Mac/Unix and Windows

--- dash/download_data_files.py
+++ dash/download_data_files.py
@@ -17,8 +17,8 @@ def download_all_files():
 
     urlpaths = ["https://raw.githubusercontent.com/daniel-muthukrishna/DASH/master/dash/model_trainedAtZeroZ.ckpt",
                 "https://raw.githubusercontent.com/daniel-muthukrishna/DASH/master/dash/type_age_atRedshiftZero.npz",
-                "https://raw.githubusercontent.com/daniel-muthukrishna/DASH/master/dash/type_age_atRedshiftZero.npz",
-                "https://raw.githubusercontent.com/daniel-muthukrishna/DASH/master/dash/training_params.pickle"]
+                "https://raw.githubusercontent.com/daniel-muthukrishna/DASH/master/dash/training_params.pickle",
+                "https://raw.githubusercontent.com/daniel-muthukrishna/DASH/master/dash/templates.npz"]
 
     printStatuses = ["Downloading Trained Model...", "Downloading Training Data files...",
                      "Downloading Model Parameters File...", "Downloading Template Data files..."]
