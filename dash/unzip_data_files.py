1e95fbf5cfb5369fddd98bb4e8680ff314263332
danmuth <daniel.muthukrishna@gmail.com>
2017-06-08 16:02:57 +1000

    Unzipping correct model_v01.zip file.

--- dash/unzip_data_files.py
+++ dash/unzip_data_files.py
@@ -4,7 +4,7 @@ import os
 
 def unzip_data_files(dataZipFilename):
     scriptDirectory = os.path.dirname(os.path.abspath(__file__))
-    zipRef = zipfile.ZipFile(os.path.join(scriptDirectory, dataZipFilename, 'r')
+    zipRef = zipfile.ZipFile(os.path.join(scriptDirectory, dataZipFilename, 'r'))
     zipRef.extractall(os.path.join(scriptDirectory, dataZipFilename.strip(".zip")))
     zipRef.close()
     os.remove(os.path.join(scriptDirectory, dataZipFilename))
