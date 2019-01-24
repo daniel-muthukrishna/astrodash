bfc6102f48ead1087ae82dc1bf1f6fc6a6c52c7d
daniel-muthukrishna <daniel.muthukrishna@gmail.com>
2019-01-24 14:45:31 +0000

    Comment out plotting

--- astrodash/false_positive_rejection.py
+++ astrodash/false_positive_rejection.py
@@ -118,20 +118,6 @@ class RlapCalc(object):
 
         xCorrNormRearranged = np.concatenate((xCorrNorm[int(len(xCorrNorm) / 2):], xCorrNorm[0:int(len(xCorrNorm) / 2)]))
 
-        #
-        # w0 = 2500.  # wavelength range in Angstroms
-        # w1 = 11000.
-        # nw = 1024.  # number of wavelength bins
-        # dwlog = np.log(w1 / w0) / self.nw
-        # zaxisindex1 = np.concatenate((np.arange(-self.nw / 2, 0), np.arange(0, self.nw / 2)))
-        # zaxis1 = np.zeros(self.nw)
-        # zaxis1[0:(self.nw/2 -1)] = (np.exp(abs(zaxisindex1[0:(self.nw/2-1)]) * dwlog) - 1)
-        # zaxis1[self.nw/2:] = -(np.exp(abs(zaxisindex1[(self.nw/2):]) * dwlog) - 1)
-        # plt.plot(zaxis1, xCorrNormRearranged)
-        # plt.show()
-        # plt.plot(self.zAxis, np.correlate(self.inputFlux, templateFlux, mode='Full')[::-1][512:1536]/max(np.correlate(self.inputFlux, templateFlux, mode='Full')))
-        # plt.plot(self.zAxis, np.correlate(templateFlux, templateFlux, mode='Full')[::-1][512-shift:1536-shift]/max(np.correlate(templateFlux, templateFlux, mode='Full')))
-
         crossCorr = np.correlate(self.inputFlux, templateFlux, mode='Full')[::-1][int(self.nw/2):int(self.nw + self.nw/2)]/max(np.correlate(self.inputFlux, templateFlux, mode='Full'))
 
         try:
@@ -144,6 +130,32 @@ class RlapCalc(object):
         except IndexError as err:
             print("Error: Cross-correlation is zero, probably caused by empty spectrum.", err)
             rmsA = 1
+
+        # import matplotlib.pyplot as plt
+        # w0 = 3500.  # wavelength range in Angstroms
+        # w1 = 10000.
+        # nw = 1024.  # number of wavelength bins
+        # dwlog = np.log(w1 / w0) / self.nw
+        # zaxisindex1 = np.concatenate((np.arange(-self.nw / 2, 0), np.arange(0, self.nw / 2)))
+        # zaxis1 = np.zeros(self.nw)
+        # zaxis1[0:int(self.nw/2 -1)] = (np.exp(abs(zaxisindex1[0:int(self.nw/2-1)]) * dwlog) - 1)
+        # zaxis1[int(self.nw/2):] = -(np.exp(abs(zaxisindex1[int(self.nw/2):]) * dwlog) - 1)
+        # plt.figure(figsize=(10, 8))
+        # plt.plot(zaxis1-0.24, xCorrNormRearranged, label='Cross-correlation, $c(n)$')
+        # plt.plot(zaxis1-0.24, aRandomFunction/7, label='Antisymmetric component, $a(n)$', linestyle='--')
+        # plt.axhline(rmsA/7, label='Antisymmetric rms, $\sigma_a$', color='k')
+        # # plt.show()
+        # # plt.plot(self.zAxis-0.24, np.correlate(self.inputFlux, templateFlux, mode='Full')[::-1][512:1536]/max(np.correlate(self.inputFlux, templateFlux, mode='Full')))
+        # # plt.plot(self.zAxis-0.24, np.correlate(templateFlux, templateFlux, mode='Full')[::-1][512-shift:1536-shift]/max(np.correlate(templateFlux, templateFlux, mode='Full')))
+        # plt.xlabel('$z - z_{peak}$', fontsize=20)
+        # plt.ylabel('Normalised correlation amplitude', fontsize=20)
+        # plt.legend(frameon=False, fontsize=15)
+        # plt.xlim(-0.4, 0.4)
+        # plt.tick_params(labelsize=18)
+        # plt.tight_layout()
+        # plt.savefig('/Users/danmuth/OneDrive/Documents/DASH/Paper/Figures/cross_correlation_{}.pdf'.format(np.random.randint(100)))
+        # plt.show()
+
 
         return xCorr, rmsInput, rmsTemp, xCorrNorm, rmsXCorr, xCorrNormRearranged, rmsA
 
@@ -267,6 +279,7 @@ class RlapCalc(object):
 
         self.zAxis = self.get_redshift_axis(self.nw, self.dwlog)
         rlapList = []
+        print(len(self.templateNames))
         for i in range(len(self.templateNames)):
             r, lap, rlap, fom = self.rlap_score(tempIndex=i)
             rlapList.append(rlap)
